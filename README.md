ee59bf9ab8a57d00fbcc1e63a316c5d8d6fdff99
morgondag <kim@morgondag.nu>
2016-07-19 22:11:51 +0200

    initial plugin

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # react-render-webpack-plugin
-react serverside rendering for webpack. 
+react serverside rendering for webpack.
+
+
+```npm i react-render-webpack-plugin```
+
+
+``` webpack.prod.config.js ```:
+
+```js
+const reactRenderPlugin = require('react-render-webpack-plugin.js');
+module.exports = {
+    plugins: [
+        new reactRenderPlugin({
+            'file': 'src/app.jsx', // The entry file of your app
+      		'parentClass': 'app-react' // the class-selector to render into
+        })
+    ]
+}
+```
+
+
+``` index.html ```:
+```html
+<!DOCTYPE html>
+<html lang="en">
+<body>
+	<div class="app-react"></div>
+	<script src="app.min.js"></script>
+</body>
+</html>
+```
+
---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict'
+const render = require('react-node-render')
+const fs = require('fs')
+const cheerio = require('cheerio')
+
+module.exports = class {
+    constructor(options) {
+        this.options = options
+    }
+
+    compile(c, cb) {
+        const filePath = process.cwd() + '/' + this.options.file
+        let html = `<div class="app-react">${render(filePath)}</div>`
+        const indexPath = c.outputPath + '/index.html'
+
+        fs.readFile(indexPath, 'utf8', (err, data) => {
+            if (err) {
+                throw new Error(err);
+            }
+
+            let $ = cheerio.load(data)
+            html = $('.' + this.options.parentClass).html(html).html();
+
+            fs.writeFile(indexPath, $.html(), 'utf-8', (err) => {
+                if (err) {
+                    throw new Error(err);
+                }
+                callback()
+            })
+        })
+    }
+
+    apply(compiler) {
+        compiler.plugin('emit', this.compile);
+    }
+}
